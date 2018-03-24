ef60ff9abb55cb3281934a6b01338f9b6c835bc4
Duc Nguyen <minhducng93@gmail.com>
2018-03-24 14:48:21 -0400

    Modify recursion part

--- src/RecursionPartIII.java
+++ src/RecursionPartIII.java
@@ -1,4 +1,54 @@
 
 public class RecursionPartIII {
     
+    public static int trackX = -1;
+    public static boolean reverse;
+    public static int defaultLevel = 0;
+    public static char[] str = new char[1000];
+    
+    public static void pattern(int x, int y)
+    {
+	if (trackX == -1) trackX = x;
+	System.out.println(line(x));
+	if (x != y) {
+	    if (trackX == x && reverse) return;
+	    if (!reverse) pattern(x+1,y);
+	    else pattern(x-1,y);
+	} else {
+	    System.out.println(line(x));
+	    reverse = true;
+	    pattern(x-1,y);
+	}
+    }
+    
+    public static String line(int n) {
+	String str = "";
+	for (int i = 0; i<n; i++) 
+	    str += "*";
+	return str;
+    }
+   
+    
+    public static void level(String title, int levels) {
+	if (defaultLevel == 0) defaultLevel = levels;
+	for (int i = 1; i<=9; i++) {
+	    str[levels] = (char) (i + 48);
+	    if (levels == 1) {
+		String result = title;
+		for (int j = defaultLevel; j>=1; j--) {
+		    result = result + str[j] + ".";
+		}
+		System.out.println(result);
+	    } else {
+		
+		level(title, levels - 1);
+	    }
+	}
+    }
+  
+    public static void main(String[] args) {
+	pattern(3,5);
+	level("Title",3);
+    }
+    
 }
